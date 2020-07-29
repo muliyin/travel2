76cdaf073e5c7c4a6c4ed1a1d55a9f11e71da8f6
muliyin <247396282@qq.com>
2020-07-29 19:04:15 +0800

    项目初始化

---
+++ src/components/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="home">
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Home",
+
+		data(){
+			return {
+
+			}
+		},
+
+		methods:{
+
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import store from './store/store'
+import router from "./router/index"
+/*初始化样式*/
+import './assets/styles/reset.css'
+/*一像素边框*/
+import './assets/styles/border.css'
+/*解决移动端点击300ms延迟*/
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
 
+fastClick.attach(document.body)
+
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
